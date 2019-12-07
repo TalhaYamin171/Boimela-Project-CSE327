c64320a9eb57397571849eedff4e9fc4b8ce7a04
Talha Yamin <talha.yamin@northsouth.edu>
2019-12-08 01:09:23 +0600

    Added unit test for Home Page

---
+++ boimelaproject/boimelawebApp/tests/test_urls.py
@@ -0,0 +1,11 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from boimelawebApp.views import home, latest, navigation, StallListView, StallDetailView, StallCreateView, StallUpdateView, StallDeleteView
+
+# Unit Tests for site urls
+
+
+class HomeTest(SimpleTestCase):
+    def test_list_url_is_resolved(self):
+        url = reverse('boimelaApp-home')
+        self.assertEquals(resolve(url).func, home)
