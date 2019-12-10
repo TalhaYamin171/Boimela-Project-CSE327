021b7b6a70cf66cb528036517e7fdecad678f4f1
Talha Yamin <talha.yamin@northsouth.edu>
2019-12-10 17:16:48 +0600

    Added unit test for Navigation Page

--- boimelaproject/boimelawebApp/tests/test_urls.py
+++ boimelaproject/boimelawebApp/tests/test_urls.py
@@ -9,3 +9,9 @@ class HomeTest(SimpleTestCase):
     def test_list_url_is_resolved(self):
         url = reverse('boimelaApp-home')
         self.assertEquals(resolve(url).func, home)
+
+
+class NavigationTest(SimpleTestCase):
+    def test_list_url_is_resolved(self):
+        url = reverse('boimelaApp-navigation')
+        self.assertEquals(resolve(url).func, navigation)
