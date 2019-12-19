b13be7f763c065b3ac0490db5cc6148274f51d21
Talha Yamin <talha.yamin@northsouth.edu>
2019-12-19 15:37:03 +0600

    Fixed a typo in the settings file

--- boimelaproject/boimelaproject/settings.py
+++ boimelaproject/boimelaproject/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-LOGIN_REDIRECT_URL = 'boimelaApp-home'
+LOGIN_REDIRECT_URL = 'boimelawebApp-home'
 
 LOGIN_URL = 'login'
